221070529c057f2d43e8da1b19d93b6f313bafac
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-08-07 19:29:52 +0530

    pending

---
+++ src/GetRAMInfo2.java
@@ -0,0 +1,24 @@
+
+
+import oshi.SystemInfo;
+import oshi.hardware.GlobalMemory;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.FormatUtil;
+
+public class GetRAMInfo2 {
+    public static void main(String... args) {
+    	
+    	ClassLoader classLoader;
+    	
+        SystemInfo systemInfo = new SystemInfo();
+        HardwareAbstractionLayer hardware = systemInfo.getHardware();
+        GlobalMemory globalMemory = hardware.getMemory();
+        
+        long usedMemory = globalMemory.getTotal() - globalMemory.getAvailable();
+
+        System.out.println("Total memory: " + FormatUtil.formatBytes(globalMemory.getTotal()));
+        System.out.println("Available memory: " + FormatUtil.formatBytes(globalMemory.getAvailable()));
+        System.out.println("Used memory: " + FormatUtil.formatBytes(usedMemory));
+    }
+}
+
